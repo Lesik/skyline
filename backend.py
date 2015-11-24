931cd83f172d326d9d2f86b245c6cb239317e1e1
Oles Pidgornyy <pidgornyy@informatik.uni-frankfurt.de>
2015-11-24 16:20:47 +0100

    fix compare_fields() in backend

--- backend.py
+++ backend.py
@@ -9,20 +9,24 @@ __author__ = "6082200: Oles Pidgornyy, 6040608: Phillip Berger"
 
 class Backend:
 
+	field = None
 	field_dimens = 4
 
-	def compare_fields(self, correct, check):
+	def compare_fields(self, field):
 		all_correct = True
 		for row in range(self.field_dimens):
-			for column in range(self.field_dimens):
-				if not correct[row][column] == check[row][column]:
+			for col in range(self.field_dimens):
+				if not self.field[row][col] == field[row][col]:
 					all_correct = False
 		return all_correct
+
+	def get_field(self):
+		return self.field
 
 	def riilain(self, list, index, item):
 		"""Replace item in list, append if needed (RIILAIN).
 		:param list: the list in question
-		:param index: index of where item should ideally go
+		:param index: index of where item shoul  ideally go
 		:param item: item that should be inserted
 		:return: nothing
 		"""
@@ -44,17 +48,16 @@ class Backend:
 			cont = False
 			while not cont:
 				random.shuffle(heights)
-				for column in range(self.field_dimens):
-					self.riilain(field[row], column, heights[column])
+				for col in range(self.field_dimens):
+					self.riilain(field[row], col, heights[col])
 				# uncomment this to see how many tries it takes until a
 				# fitting combination was found, quite amusing
 				#print(field[row])
 
 				cont = True
-				for column in range(self.field_dimens):
+				for col in range(self.field_dimens):
 					for previous_row in range(len(field) - 1):
-						if (field[previous_row][column]	== \
-								field[row][column]):
+						if (field[previous_row][col] == field[row][col]):
 							cont = False
 			#print(field[row])
-		return field
+		self.field = field
--- debug.py
+++ debug.py
@@ -2,8 +2,9 @@
 __author__ = "6040608: Phillip Berger"
 import backend
 
-asd = backend.Backend()
-fiel = asd.generate_field()
-#print(fiel)
-aasd = input()
-#print(asd.compare_fields(fiel, aasd))
+game_backend = backend.Backend()
+game_backend.generate_field()
+print(game_backend.get_field())
+true = game_backend.compare_fields(eval(input()))
+#true = game_backend.compare_fields([[2, 3, 1, 4], [4, 1, 2, 3], [1, 4, 3, 2], [3, 2, 4, 1]])
+print(true)
